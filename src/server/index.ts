1b67f286007b6c941fce5d3e9564b3d596894d04
Marc Moreno <marcmoreno112@gmail.com>
2023-05-25 11:41:07 +0200

    Set morgan middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,21 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS!.split(" ");
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
+
+app.disable("x-powered-by");
+
+app.use(morgan("dev"));
+
+app.use(express.json());
 
 export default app;
