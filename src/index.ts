3683de67d9db28721ce97b75ddeb614fa8e5ed36
Marc Moreno <marcmoreno112@gmail.com>
2023-05-25 18:06:57 +0200

    Connection to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "./loadEnvironment.js";
 import createDebug from "debug";
 import chalk from "chalk";
 import app from "./server/index.js";
+import connectToDatabase from "./database/connectToDatabase.js";
 
 const debug = createDebug("extintores-api:root");
 
@@ -10,3 +11,19 @@ const port = process.env.PORT ?? 4000;
 app.listen(port, () => {
   debug(chalk.bgGreen(`Listening on http://localhost:${port}`));
 });
+
+const mongoDbConnection = process.env.MONGODB_CONNECTION;
+
+if (!mongoDbConnection) {
+  debug(chalk.bgRed(`Missing mongoDbConnection environment variable`));
+  process.exit(1);
+}
+
+try {
+  await connectToDatabase(mongoDbConnection);
+  debug(chalk.bgGreen("Connected to database"));
+} catch (error: unknown) {
+  debug(
+    chalk.bgRed(`Error connecting to database: ${(error as Error).message}`)
+  );
+}
---
+++ src/server/paths/paths.ts
@@ -0,0 +1,5 @@
+const paths = {
+  ping: "/",
+};
+
+export default paths;
