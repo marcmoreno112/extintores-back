c49334580b31d194e4ccf70ae90507d40ba15916
Marc Moreno <marcmoreno112@gmail.com>
2023-05-25 14:14:13 +0200

    Create notFoundError middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { generalError, notFoundError } from "./middlewares/errorMiddlewares.js";
 
 const app = express();
 
@@ -17,5 +18,9 @@ app.disable("x-powered-by");
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use(notFoundError);
+
+app.use(generalError);
 
 export default app;
