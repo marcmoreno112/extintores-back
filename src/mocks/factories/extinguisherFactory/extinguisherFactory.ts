c82a31dd181ff58163d981556d47f79a7827fe4a
Marc Moreno <marcmoreno112@gmail.com>
2023-06-03 18:39:30 +0200

    Create extinguishersFactory

---
+++ src/mocks/factories/extinguisherFactory/extinguisherFactory.ts
@@ -0,0 +1,28 @@
+import { Factory } from "fishery";
+import { type ExtinguisherData } from "../../../types.js";
+import { faker } from "@faker-js/faker";
+
+const extinguisherFactory = Factory.define<ExtinguisherData>(() => ({
+  id: faker.database.mongodbObjectId().toString(),
+  user: faker.database.mongodbObjectId().toString(),
+  brand: faker.company.name(),
+  class: faker.helpers.arrayElements(["A", "B", "C", "D", "K"], {
+    min: 1,
+    max: 3,
+  }),
+  description: faker.commerce.productDescription(),
+  disadvantages: faker.commerce.productDescription(),
+  fireExtinguishingAgent: faker.commerce.productDescription(),
+  img: faker.image.url(),
+  model: faker.commerce.product(),
+  strengths: faker.commerce.productDescription(),
+  usefulLife: faker.string.sample(),
+}));
+
+export const extinguisherMock = (data?: ExtinguisherData) =>
+  extinguisherFactory.build(data);
+
+export const extinguishersMock = (
+  numberOfExtinguishers: number,
+  data?: ExtinguisherData
+) => extinguisherFactory.buildList(numberOfExtinguishers, data);
---
+++ src/types.ts
@@ -0,0 +1,43 @@
+import { type Request } from "express";
+import { type Types } from "mongoose";
+
+export interface UserCredentials {
+  username: string;
+  password: string;
+}
+
+export interface DbUser extends UserCredentials {
+  name: string;
+}
+
+export type UserCredentialsRequest = Request<
+  Record<string, unknown>,
+  Record<string, unknown>,
+  UserCredentials
+>;
+
+export interface UserDataStructure extends UserCredentials {
+  _id: string;
+}
+
+export interface UserData extends UserCredentials {
+  _id: Types.ObjectId;
+}
+
+export interface ExtinguisherData {
+  id: string;
+  brand: string;
+  model: string;
+  class: string[];
+  img: string;
+  usefulLife: string;
+  fireExtinguishingAgent: string;
+  description: string;
+  disadvantages: string;
+  strengths: string;
+  user: string;
+}
+
+export interface CustomRequest extends Request {
+  userId: string;
+}
