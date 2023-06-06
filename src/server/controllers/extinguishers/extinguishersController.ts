ce769df8dd762091b4058d16bf9c8f1d6d45378b
Marc Moreno <marcmoreno112@gmail.com>
2023-06-07 00:30:28 +0200

    Create deleteExtinguisher controller

--- src/server/controllers/extinguishers/extinguishersController.ts
+++ src/server/controllers/extinguishers/extinguishersController.ts
@@ -1,5 +1,6 @@
 import { type NextFunction, type Request, type Response } from "express";
 import Extinguisher from "../../../database/models/Extinguisher.js";
+import CustomError from "../../CustomError/CustomError.js";
 
 export const getExtinguishers = async (
   req: Request,
@@ -14,6 +15,30 @@ export const getExtinguishers = async (
     res.status(status).json({ extinguishers });
   } catch {
     const error = new Error("Database error");
+    next(error);
+  }
+};
+
+export const deleteExtinguisher = async (
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const extinguisher = await Extinguisher.findByIdAndDelete(id).exec();
+
+    if (!extinguisher) {
+      const error = new CustomError("Extinguisher not found", 404);
+
+      throw error;
+    }
+
+    res.status(200).json({
+      message: `Extinguisher deleted`,
+    });
+  } catch (error) {
     next(error);
   }
 };
